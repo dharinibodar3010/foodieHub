42bac5168364941d6fe16d3d8bf8d992a5646a66
dharinibodar3010 <dharinibodar3010@gmail.com>
2026-07-25 02:03:51 +0530

    commit

--- FoodieHub/src/main/java/com/example/FoodieHubApplication.java
+++ FoodieHub/src/main/java/com/example/FoodieHubApplication.java
@@ -6,9 +6,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 
 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 @SpringBootApplication
 @EnableAsync
-public class FoodieHubApplication {
+public class FoodieHubApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FoodieHubApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodieHubApplication.class, args);
